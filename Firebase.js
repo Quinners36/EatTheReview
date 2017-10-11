1acd82c62f2c483645cc71374c8bf60d8ce16270
Stephen Kane <stephen.kane.2016@mumail.ie>
2017-10-11 12:53:45 +0100

    Making Firebase file

---
+++ Firebase.js
@@ -0,0 +1,18 @@
+
+
+
+//User registering
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+//User Signing in
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
