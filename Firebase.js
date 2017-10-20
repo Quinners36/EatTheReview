a247403c14c78c67bdf53488adfaa68f2b381946
Stephen Kane <stephen.kane.2016@mumail.ie>
2017-10-20 15:52:02 +0100

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -9,15 +9,27 @@
   };
   firebase.initializeApp(config);
 
+$(document).ready(function(){
 
 
 //User registering
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  
+$('#signup').click(function(){
+
+    var email=$('#signupemail').val();
+    var password=$('#signuppassword').val();
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        console.log(error.code);
+        console.log(error.message);
+        // ...
+    });
+
 });
+
+});
+
+
 
 //User Signing in
 firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
