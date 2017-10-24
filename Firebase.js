ff23f496e0f282db9337a197f05687ed143a1d52
Stephen Kane <stephen.kane.2016@mumail.ie>
2017-10-24 21:41:45 +0100

    test

--- Firebase.js
+++ Firebase.js
@@ -1,53 +1,55 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD07XUsubZBt2kGhyG2xPOfOq49UZTyaw4",
-    authDomain: "http://eatthereview.openode.io/",
-    databaseURL: "https://eat-the-review.firebaseio.com",
-    projectId: "eat-the-review",
-    storageBucket: "",
-    messagingSenderId: "847435972429"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD07XUsubZBt2kGhyG2xPOfOq49UZTyaw4",
+  authDomain: "http://eatthereview.openode.io/",
+  databaseURL: "https://eat-the-review.firebaseio.com",
+  projectId: "eat-the-review",
+  storageBucket: "",
+  messagingSenderId: "847435972429"
+};
+firebase.initializeApp(config);
 
 $(document).ready(function(){
 
 
 //User registering
-$('#signupbutton').click(function(){
+  $('#signupbutton').click(function(){
 
-    var email=$('#signupemail').val();
-    var password=$('#signuppassword').val();
+      var email=$('#signupemail').val();
+      var password=$('#signuppassword').val();
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        console.log(error.code);
-        console.log(error.message);
-        // ...
-    });
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          console.log(error.code);
+          console.log(error.message);
+          $('#errorarea').html(error.code +'<br>'+ error.message);
+          // ...
+      });
 
-});
-
-
-
-
-//User Signing in
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    window.location="RoughHomePage.html";
-  } else {
-    // No user is signed in.
-  }
-});
-  
-  
-  
-  
   });
+
+  $('#Login').click(function(){
+
+      var email=$('#loginemail').val();
+      var password=$('#loginpassword').val();
+
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          $('#errorarea2').html(error.code +'<br>'+ error.message);
+          // ...
+      });
+
+  });
+
+
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+          window.location = 'RoughHomePage.html';
+      } else {
+          // No user is signed in.
+      }
+  });
+});
+ //Stephen Waz ere
