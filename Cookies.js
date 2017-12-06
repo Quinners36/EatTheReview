6f47f99a17b6a3c2760c14705617059c3ebb9d34
Stephen Kane <stephen.kane.2016@mumail.ie>
2017-12-06 01:01:38 +0000

    Cookies

--- Cookies.js
+++ Cookies.js
@@ -1,21 +1,17 @@
 
 
-$(document).ready(function(){
-
-//User registering
-
-    function setCookie(cvalue) {
+function setCookie(cvalue) {
     var d = new Date();
     d.setTime(d.getTime() + (1*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie ="ResterauntID=" + cvalue + ";" + expires + ";path=/";
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = 'RestID' + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+    for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
@@ -28,15 +24,10 @@ function getCookie(cname) {
 }
 
 function checkCookie() {
-    var ID = getCookie("ResterauntID");
-    if (ID != "") {
-        console.log("No Visisted Resteraunts")
+    var user=getCookie("RestID");
+    if (user != "") {
+        return user;
     } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("ResterauntID", user);
-        }
-    }
-}
 
-  });
+       }
+    }
